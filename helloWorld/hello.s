dc5bad0392c23710272e2658a2beba7c1cb4ce0b
SRE <seansre@gmail.com>
2020-07-20 22:20:08 -0500

    added loop to repeatedly print message

--- helloWorld/hello.s
+++ helloWorld/hello.s
@@ -1,17 +1,24 @@
+# Give a greeting 10 times
  .global _start
 
-        .text
+ .text
 _start:
+        mov     $10, %rbp
+
+write:
         # write(1, message, 13)
+
         mov     $1, %rax
         mov     $1, %rdi
         mov     $message, %rsi
         mov     $13, %rdx
         syscall
+        dec     %rbp
+        jg      write
 
         # exit(0)
         mov     $60, %rax
-        xor     %rdi, %rdi
+        mov     $0, %rdi
         syscall
 
 message:
